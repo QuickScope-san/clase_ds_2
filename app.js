d2593bec70fa6934b95c55ab1660a9265c20fbb1
Dilan Joel Bergano Caicedo <=>
2022-05-03 19:23:42 -0500

    Creación de endpoint pares

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const operation = require('./utils/operations');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/pares', (req, res) => {
+  const {numero} = req.body;
+  res.send(operation.pares(numero));
 })
 
 app.listen(port, () => {
