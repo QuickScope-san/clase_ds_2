e0f5ac54a4beaa95d84c16ef18073b979a849efa
DavidHume1711 <felipeguerrasaenz@gmail.com>
2022-05-03 21:45:47 -0500

    FACTORIAL

--- utils/operations.js
+++ utils/operations.js
@@ -1,22 +1,40 @@
 let pares = (x) => {
-    let resp = ""
-    if (x<2){
-     resp="Los numeros son : 0"
-     return resp
-    }
-
-   resp = "Los numeros son :"
-   let i = 0;
-   do {
-   if ( i % 2 == 0){
-    resp = resp + ` ${i}`
-   }   
-   i = i + 1;
-   } while (i <= x);
-
+  let resp = "";
+  if (x < 2) {
+    resp = "Los numeros son : 0";
     return resp;
- }
+  }
 
-module.exports={
-    pares,
-}
+  resp = "Los numeros son :";
+  let i = 0;
+  do {
+    if (i % 2 == 0) {
+      resp = resp + ` ${i}`;
+    }
+    i = i + 1;
+  } while (i <= x);
+
+  return resp;
+};
+
+// Función Factorial realizada por Felipe Guerra Sáenz
+let factorial = (n) => {
+  let answer = 1;
+
+  if (n == 0 || n == 1) {
+    return answer;
+  } else {
+    for (let i = n; i >= 1; i--) {
+      answer = answer * i;
+    }
+    return answer;
+  }
+};
+//let n = 4;
+//answer = factorial(n);
+//console.log("The factorial of " + n + " is " + answer);
+
+module.exports = {
+  pares,
+  factorial,
+};
